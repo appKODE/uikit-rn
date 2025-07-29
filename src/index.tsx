70be08ac5559bcdb46a6a59ee47ede5aa7ca9889
@ca <aleksandr.nikolaevich.ch@ya.ru>
2025-07-30 00:26:47 +0300

    feat: added export for hooks

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@ export { createThemedApp } from './ui-kit';
 export { dark, defaultConfig, light } from './defaults';
 export type { DefaultTheme } from './types';
 export { StyleSheet } from 'react-native-unistyles';
+export { useAsyncAction, useTheme } from './hooks';
