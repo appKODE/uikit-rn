98e9b0686904db1515b121e967419d2e40825b69
@ca <aleksandr.nikolaevich.ch@ya.ru>
2025-07-30 09:55:21 +0300

    fix: fixed exporting types

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 export { createThemedApp } from './ui-kit';
 export { dark, defaultConfig, light } from './defaults';
-export type { DefaultTheme } from './types';
-export { StyleSheet } from 'react-native-unistyles';
+export * from './types';
+export { StyleSheet, UnistylesRuntime } from 'react-native-unistyles';
 export { useAsyncAction, useTheme } from './hooks';
