21e3736ac56549224459abf018874d3d7455cfac
Anton Gomonchuk <76778903+trysted@users.noreply.github.com>
2025-08-26 17:28:06 +0300

    fix: Переписал Button Loader на reanimated

---
+++ src/components/buttons/button-loader.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import { StyleSheet } from 'react-native-unistyles';
+
+import { ActivityIndicator, DEFAULT_DURATION } from '../../primitives';
+import type { IconColorFullKeys } from '../../types';
+
+type Props = {
+  iconColor: IconColorFullKeys;
+  isLoading: boolean;
+};
+
+export const ButtonLoader = ({ iconColor, isLoading }: Props) => {
+  const progress = useSharedValue(0);
+
+  useEffect(() => {
+    progress.value = withTiming(isLoading ? 1 : 0, {
+      duration: DEFAULT_DURATION,
+    });
+  }, [isLoading, progress]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: progress.value,
+    transform: [{ scale: 0.7 + 0.3 * progress.value }],
+  }));
+
+  return (
+    <Animated.View style={[styles.loader, animatedStyle]}>
+      <ActivityIndicator animating={isLoading} color={iconColor} />
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  loader: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
