f4a277d83644d6f66077c7536865e8361eb81af3
leikaa <46675254+leikaa@users.noreply.github.com>
2025-09-25 21:21:23 +0300

    feat(select-field-ref): добавлена возможность использовать ref для вызова закрытия bottom sheet (#16)
    * feat(select-field-ref): добавлена возможность использовать ref для вызова закрытия bottom sheet
    
    * chore(select-field-ref): добавлен import JSX
    
    ---------
    
    Co-authored-by: Vyacheslav Sludsky <vsludsky@i-neti.ru>

--- src/components/form-fields/index.ts
+++ src/components/form-fields/index.ts
@@ -1,3 +1,7 @@
 export { TextField, type TextFieldProps } from './text-field';
 export { TextAreaField, type TextAreaFieldProps } from './text-area-field';
-export { SelectField, type SelectFieldProps } from './select-field';
+export {
+  SelectField,
+  type SelectFieldProps,
+  type SelectFieldRef,
+} from './select-field';
